0112f874b8f469c8f3946ab7674ff7d532dd038d
andrewsykim <kim.andrewsy@gmail.com>
2017-12-27 13:11:51 -0500

    skip build/push if user is not cloudnativelabs since it cannot access dockerhub creds

--- build/travis-deploy.sh
+++ build/travis-deploy.sh
@@ -5,6 +5,10 @@ set -o pipefail
 # Pull Request image tag format: PR00
 if [ "${TRAVIS_EVENT_TYPE}" = "pull_request" ]; then
     PR_USER=$(echo "$TRAVIS_PULL_REQUEST_SLUG" | sed -e 's/\/.*//')
+    if [ "$PR_USER" != "cloudnativelabs" ]; then
+      echo "Not building/pushing PR $TRAVIS_PULL_REQUEST since only the cloudnativelabs user can access docker hub credentials"
+      exit 0
+    fi
     echo "Building/pushing PR$TRAVIS_PULL_REQUEST from $PR_USER"
     make push IMG_TAG="PR$TRAVIS_PULL_REQUEST"
     exit 0
