987943d126e392a361481fd9911ac9fe77e62c20
Joakim Karlsson <joakim@roffe.nu>
2018-01-22 00:52:19 +0100

    fix

--- app/controllers/metrics_controller.go
+++ app/controllers/metrics_controller.go
@@ -124,6 +124,7 @@ type MetricsController struct {
 
 // Run prometheus metrics controller
 func (mc *MetricsController) Run(stopCh <-chan struct{}, wg *sync.WaitGroup) error {
+	var err error
 	t := time.NewTicker(mc.syncPeriod)
 	defer t.Stop()
 	defer wg.Done()
