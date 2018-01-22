4cddb1b5ca1d0739358c84a8334fcbd9b735a0aa
Joakim Karlsson <joakim@roffe.nu>
2018-01-22 14:11:35 +0100

    test

--- app/options/options.go
+++ app/options/options.go
@@ -40,7 +40,7 @@ type KubeRouterConfig struct {
 	MetricsPort         uint16
 	MetricsPath         string
 	MetricsSyncPeriod   time.Duration
-	VLevel              int
+	VLevel              string
 	// FullMeshPassword    string
 }
 
@@ -119,6 +119,6 @@ func (s *KubeRouterConfig) AddFlags(fs *pflag.FlagSet) {
 	fs.DurationVar(&s.MetricsSyncPeriod, "metrics-sync-period", s.MetricsSyncPeriod, "The delay between exporting metrics (e.g. '5s', '1m', '2h22m'). Must be greater than 0.")
 	// fs.StringVar(&s.FullMeshPassword, "nodes-full-mesh-password", s.FullMeshPassword,
 	// 	"Password that cluster-node BGP servers will use to authenticate one another when \"--nodes-full-mesh\" is set.")
-	fs.IntVarP(&s.VLevel, "v", "v", 0, "log level for V logs")
+	fs.StringVarP(&s.VLevel, "v", "v", "0", "log level for V logs")
 
 }
