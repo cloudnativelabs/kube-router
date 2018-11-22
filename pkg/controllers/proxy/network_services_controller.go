98872fb4f6ca03b24f61fc909e1c63c7f5928d6c
Joakim Karlsson <joakim@roffe.nu>
2018-11-22 19:58:02 +0100

    custom error type

--- pkg/controllers/proxy/network_services_controller.go
+++ pkg/controllers/proxy/network_services_controller.go
@@ -277,39 +277,63 @@ func (nsc *NetworkServicesController) Run(healthChan chan<- *healthcheck.Control
 	}
 	// https://www.kernel.org/doc/Documentation/networking/ipvs-sysctl.txt
 	// enable ipvs connection tracking
-	err = utils.SetSysctl("net/ipv4/vs/conntrack", 1)
-	if err != nil {
-		return errors.New("Failed to set net.ipv4.vs.conntrack=1 due to: %s" + err.Error())
+	errs := utils.SetSysctl("net/ipv4/vs/conntrack", 1)
+	if errs != nil {
+		if errs.IsFatal() {
+			return errors.New(errs.Error())
+		} else {
+			glog.Info(errs.Error)
+		}
 	}
 
 	// LVS failover not working with UDP packets https://access.redhat.com/solutions/58653
-	err = utils.SetSysctl("net/ipv4/vs/expire_nodest_conn", 1)
-	if err != nil {
-		return errors.New("Failed to set net.ipv4.vs.expire_nodest_conn=1 due to: %s" + err.Error())
+	errs = utils.SetSysctl("net/ipv4/vs/expire_nodest_conn", 1)
+	if errs != nil {
+		if errs.IsFatal() {
+			return errors.New(errs.Error())
+		} else {
+			glog.Info(errs.Error)
+		}
 	}
 
 	// LVS failover not working with UDP packets https://access.redhat.com/solutions/58653
-	err = utils.SetSysctl("net/ipv4/vs/expire_quiescent_templat", 1)
-	if err != nil {
-		return errors.New("Failed to set net.ipv4.vs.expire_quiescent_template=1 due to: %s" + err.Error())
+	errs = utils.SetSysctl("net/ipv4/vs/expire_quiescent_templat", 1)
+	if errs != nil {
+		if errs.IsFatal() {
+			return errors.New(errs.Error())
+		} else {
+			glog.Info(errs.Error)
+		}
 	}
 
 	// https://github.com/kubernetes/kubernetes/pull/71114
-	err = utils.SetSysctl("net/ipv4/vs/conn_reuse_mode", 0)
-	if err != nil {
-		return fmt.Errorf("failed to set net.ipv4.vs.conn_reuse_mode=0 due to: %s", err)
+	errs = utils.SetSysctl("net/ipv4/vs/conn_reuse_mode", 0)
+	if errs != nil {
+		if errs.IsFatal() {
+			return errors.New(errs.Error())
+		} else {
+			glog.Info(errs.Error)
+		}
 	}
 
 	// https://github.com/kubernetes/kubernetes/pull/70530/files
-	err = utils.SetSysctl("net/ipv4/conf/all/arp_ignore", 1)
-	if err != nil {
-		return fmt.Errorf("failed to set net.ipv4.conf.all.arp_ignore=1 due to: %s", err)
+	errs = utils.SetSysctl("net/ipv4/conf/all/arp_ignore", 1)
+	if errs != nil {
+		if errs.IsFatal() {
+			return errors.New(errs.Error())
+		} else {
+			glog.Info(errs.Error)
+		}
 	}
 
 	// https://github.com/kubernetes/kubernetes/pull/70530/files
-	err = utils.SetSysctl("net/ipv4/conf/all/arp_announce", 2)
-	if err != nil {
-		return fmt.Errorf("failed to set net.ipv4.conf.all.arp_announce=2 due to: %s", err)
+	errs = utils.SetSysctl("net/ipv4/conf/all/arp_announce", 2)
+	if errs != nil {
+		if errs.IsFatal() {
+			return errors.New(errs.Error())
+		} else {
+			glog.Info(errs.Error)
+		}
 	}
 
 	// loop forever unitl notified to stop on stopCh
--- pkg/utils/sysctl.go
+++ pkg/utils/sysctl.go
@@ -5,20 +5,37 @@ import (
 	"io/ioutil"
 	"os"
 	"strconv"
-
-	"github.com/golang/glog"
 )
 
-func SetSysctl(path string, value int) error {
+type SysctlError struct {
+	err    string
+	option string
+	value  int
+	fatal  bool
+}
 
+// Error return the error as string
+func (e *SysctlError) Error() string {
+	return fmt.Sprintf("Sysctl %s=%d : %s", e.option, e.value, e.err)
+}
+
+// IsFatal was the error fatal and reason to exit kube-router
+func (e *SysctlError) IsFatal() bool {
+	return e.fatal
+}
+
+// SetSysctl sets a sysctl value
+func SetSysctl(path string, value int) *SysctlError {
 	sysctlPath := fmt.Sprintf("/proc/sys/%s", path)
 	if _, err := os.Stat(sysctlPath); err != nil {
 		if os.IsNotExist(err) {
-			glog.Infof("%s not found, could not set value: %s, Does your kernel support this feature?", sysctlPath)
-			return nil
+			return &SysctlError{"option not found, Does your kernel version support this feature?", path, value, false}
 		}
-		glog.Errorf("error stating: %s : %s", sysctlPath, err.Error())
-		return nil
+		return &SysctlError{"stat error: " + err.Error(), path, value, true}
 	}
-	return ioutil.WriteFile(sysctlPath, []byte(strconv.Itoa(value)), 0640)
+	err := ioutil.WriteFile(sysctlPath, []byte(strconv.Itoa(value)), 0640)
+	if err != nil {
+		return &SysctlError{"could not set due to: " + err.Error(), path, value, true}
+	}
+	return nil
 }
