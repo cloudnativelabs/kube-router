771719d522c8ea9305106b417506d5454ba4d3f2
Lucas Servén Marín <lserven@gmail.com>
2019-04-03 16:10:22 +0200

    docs,pkgs: change 'can not' to 'cannot'
    This commit fixes a grammar mistake in a doc and some log messages.

--- pkg/controllers/routing/bgp_peers.go
+++ pkg/controllers/routing/bgp_peers.go
@@ -39,7 +39,7 @@ func (nrc *NetworkRoutingController) syncInternalPeers() {
 	// get the current list of the nodes from API server
 	nodes, err := nrc.clientset.CoreV1().Nodes().List(metav1.ListOptions{})
 	if err != nil {
-		glog.Errorf("Failed to list nodes from API server due to: %s. Can not perform BGP peer sync", err.Error())
+		glog.Errorf("Failed to list nodes from API server due to: %s. Cannot perform BGP peer sync", err.Error())
 		return
 	}
 	if nrc.MetricsEnabled {
