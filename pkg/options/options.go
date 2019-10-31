11be33cda8c00d762e283bfcfb7a255a72189631
Aaron Layfield <aaron.layfield@gmail.com>
2019-10-31 09:34:28 +0900

    Updated docstring on ipvs-deny-all

--- pkg/options/options.go
+++ pkg/options/options.go
@@ -110,7 +110,7 @@ func (s *KubeRouterConfig) AddFlags(fs *pflag.FlagSet) {
 	fs.BoolVar(&s.IpvsGracefulTermination, "ipvs-graceful-termination", false,
 		"Enables the experimental IPVS graceful terminaton capability")
 	fs.BoolVar(&s.IpvsDenyAll, "ipvs-deny-all", false,
-		"Enables rule to accept all incoming traffic on the node. Default: true.")
+		"Enables rule to accept all incoming traffic on the node. Default: false.")
 	fs.DurationVar(&s.RoutesSyncPeriod, "routes-sync-period", s.RoutesSyncPeriod,
 		"The delay between route updates and advertisements (e.g. '5s', '1m', '2h22m'). Must be greater than 0.")
 	fs.BoolVar(&s.AdvertiseClusterIp, "advertise-cluster-ip", false,
