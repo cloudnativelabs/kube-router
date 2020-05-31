db5a8b6f07d0bb51d53071182ff465c55f4f4fbe
Manuel Rüger <mrueg@rueg.eu>
2020-05-31 17:33:19 +0200

    kube-router_test.go: defer waitgroup
    Co-authored-by: Aaron U'Ren <aauren@users.noreply.github.com>

--- cmd/kube-router/kube-router_test.go
+++ cmd/kube-router/kube-router_test.go
@@ -18,11 +18,11 @@ func TestMainHelp(t *testing.T) {
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		_, err := io.Copy(stderrBuf, stderrR)
 		if err != nil {
 			panic(err)
 		}
-		wg.Done()
 	}()
 
 	origArgs := os.Args
