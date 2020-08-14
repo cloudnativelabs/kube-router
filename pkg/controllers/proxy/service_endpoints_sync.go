7613a735de41aadc21471ce806abbd8296154204
Qingkun Li <qingkun.li@bytedance.com>
2020-08-14 07:11:09 -0700

    add IfaceHasNoAddr check for external ip delete error (#971)

--- pkg/controllers/proxy/service_endpoints_sync.go
+++ pkg/controllers/proxy/service_endpoints_sync.go
@@ -309,7 +309,7 @@ func (nsc *NetworkServicesController) setupExternalIPServices(serviceInfoMap ser
 
 				// ensure VIP less director. we dont assign VIP to any interface
 				err = nsc.ln.ipAddrDel(dummyVipInterface, externalIP)
-				if err != nil {
+				if err != nil && err.Error() != IfaceHasNoAddr {
 					glog.Errorf("Failed to delete external ip address from dummyVipInterface due to %s", err)
 					continue
 				}
