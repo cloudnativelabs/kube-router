88a76bd67604704b2b63a40fc1d71e8a32fd8bb2
Aaron U'Ren <aauren@users.noreply.github.com>
2021-05-17 12:04:28 -0500

    fix(NPC): parse NodePorts as unsigned ints
    Also separates logic so that it can be tested more easily, and adds unit
    tests to make sure there is no regression.
    
    Fixes #1083

---
+++ pkg/controllers/netpol/utils.go
@@ -0,0 +1,30 @@
+package netpol
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+func validateNodePortRange(nodePortOption string) (string, error) {
+	nodePortValidator := regexp.MustCompile(`^([0-9]+)[:-]([0-9]+)$`)
+	if matched := nodePortValidator.MatchString(nodePortOption); !matched {
+		return "", fmt.Errorf("failed to parse node port range given: '%s' please see specification in help text", nodePortOption)
+	}
+	matches := nodePortValidator.FindStringSubmatch(nodePortOption)
+	if len(matches) != 3 {
+		return "", fmt.Errorf("could not parse port number from range given: '%s'", nodePortOption)
+	}
+	port1, err := strconv.ParseUint(matches[1], 10, 16)
+	if err != nil {
+		return "", fmt.Errorf("could not parse first port number from range given: '%s'", nodePortOption)
+	}
+	port2, err := strconv.ParseUint(matches[2], 10, 16)
+	if err != nil {
+		return "", fmt.Errorf("could not parse second port number from range given: '%s'", nodePortOption)
+	}
+	if port1 >= port2 {
+		return "", fmt.Errorf("port 1 is greater than or equal to port 2 in range given: '%s'", nodePortOption)
+	}
+	return fmt.Sprintf("%d:%d", port1, port2), nil
+}
