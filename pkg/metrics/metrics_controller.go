b05d1e4bb8557838da538e6358ce42ed7e186a11
Jason Piper <j.piper@me.com>
2022-02-04 16:39:33 +0000

    one more

--- pkg/metrics/metrics_controller.go
+++ pkg/metrics/metrics_controller.go
@@ -169,7 +169,7 @@ func (mc *Controller) Run(healthChan chan<- *healthcheck.ControllerHeartbeat, st
 	prometheus.MustRegister(BuildInfo)
 	prometheus.MustRegister(ControllerIpvsMetricsExportTime)
 
-	srv := &http.Server{Addr: mc.MetricsAddr + strconv.Itoa(int(mc.MetricsPort)), Handler: http.DefaultServeMux}
+	srv := &http.Server{Addr: mc.MetricsAddr + ":" + strconv.Itoa(int(mc.MetricsPort)), Handler: http.DefaultServeMux}
 
 	// add prometheus handler on metrics path
 	http.Handle(mc.MetricsPath, promhttp.Handler())
@@ -198,7 +198,7 @@ func (mc *Controller) Run(healthChan chan<- *healthcheck.ControllerHeartbeat, st
 // NewMetricsController returns new MetricController object
 func NewMetricsController(config *options.KubeRouterConfig) (*Controller, error) {
 	mc := Controller{}
-	mc.MetricsAddr = config.MetricsAddr + ":"
+	mc.MetricsAddr = config.MetricsAddr
 	mc.MetricsPath = config.MetricsPath
 	mc.MetricsPort = config.MetricsPort
 	return &mc, nil
