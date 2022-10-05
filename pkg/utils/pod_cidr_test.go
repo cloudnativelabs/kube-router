c144d9d75b0fbb081492b4a30d966f1abded0256
Aaron U'Ren <aauren@gmail.com>
2022-10-05 00:20:57 -0500

    feat(pod_cidr): handle multiple pod CIDRs

--- pkg/utils/pod_cidr_test.go
+++ pkg/utils/pod_cidr_test.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net"
-	"os"
 	"reflect"
 	"testing"
 
@@ -13,114 +11,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 )
-
-func Test_GetPodCidrFromCniSpec(t *testing.T) {
-	testcases := []struct {
-		name        string
-		cniConfFile string
-		podCidr     net.IPNet
-		err         error
-		filename    string
-	}{
-		{
-			"CNI config file has subnet",
-			`{"bridge":"kube-bridge","ipam":{"subnet":"172.17.0.0/24","type":"host-local"},"isDefaultGateway":true,"name":"kubernetes","type":"bridge"}`,
-			net.IPNet{
-				IP:   net.IP{172, 17, 0, 0},
-				Mask: net.CIDRMask(24, 32),
-			},
-			nil,
-			"10-kuberouter.conf",
-		},
-		{
-			"CNI config file missing subnet",
-			`{"bridge":"kube-bridge","ipam":{"type":"host-local"},"isDefaultGateway":true,"name":"kubernetes","type":"bridge"}`,
-			net.IPNet{},
-			nil,
-			"10-kuberouter.conf",
-		},
-	}
-
-	for _, testcase := range testcases {
-		t.Run(testcase.name, func(t *testing.T) {
-			file, err := createFile(testcase.cniConfFile, testcase.filename)
-			if err != nil {
-				t.Fatalf("Failed to create temporary CNI config file: %v", err)
-			}
-			defer os.Remove(file.Name())
-
-			cidr, err := GetPodCidrFromCniSpec(file.Name())
-			if !reflect.DeepEqual(err, testcase.err) {
-				t.Logf("actual error: %v", err)
-				t.Logf("expected error: %v", testcase.err)
-				t.Error("did not get expected error")
-			}
-
-			if !reflect.DeepEqual(cidr, testcase.podCidr) {
-				t.Logf("actual pod cidr: %v", cidr)
-				t.Logf("expected pod cidr: %v", testcase.podCidr)
-				t.Error("did not get expected pod cidr")
-			}
-		})
-	}
-}
-
-func Test_InsertPodCidrInCniSpec(t *testing.T) {
-	testcases := []struct {
-		name        string
-		podCidr     string
-		existingCni string
-		newCni      string
-		err         error
-		filename    string
-	}{
-		{
-			"insert cidr to cni config",
-			"172.17.0.0/24",
-			`{"bridge":"kube-bridge","ipam":{"type":"host-local"},"isDefaultGateway":true,"name":"kubernetes","type":"bridge"}`,
-			`{"bridge":"kube-bridge","ipam":{"subnet":"172.17.0.0/24","type":"host-local"},"isDefaultGateway":true,"name":"kubernetes","type":"bridge"}`,
-			nil,
-			"/tmp/10-kuberouter.conf",
-		},
-		{
-			"insert cidr to cni config",
-			"172.17.0.0/24",
-			`{"cniVersion":"0.3.0","name":"mynet","plugins":[{"bridge":"kube-bridge","ipam":{"type":"host-local"},"isDefaultGateway":true,"name":"kubernetes","type":"bridge"},{"type":"portmap"}]}`,
-			`{"cniVersion":"0.3.0","name":"mynet","plugins":[{"bridge":"kube-bridge","ipam":{"subnet":"172.17.0.0/24","type":"host-local"},"isDefaultGateway":true,"name":"kubernetes","type":"bridge"},{"type":"portmap"}]}`,
-			nil,
-			"/tmp/10-kuberouter.conflist",
-		},
-	}
-
-	for _, testcase := range testcases {
-		t.Run(testcase.name, func(t *testing.T) {
-			cniConfigFile, err := createFile(testcase.existingCni, testcase.filename)
-
-			if err != nil {
-				t.Fatalf("failed to create temporary CNI config: %v", err)
-			}
-			defer os.Remove(cniConfigFile.Name())
-
-			err = InsertPodCidrInCniSpec(cniConfigFile.Name(), testcase.podCidr)
-			if !reflect.DeepEqual(err, testcase.err) {
-				t.Logf("actual error: %v", err)
-				t.Logf("expected error: %v", err)
-				t.Error("did not get expected error")
-			}
-
-			newContent, err := readFile(cniConfigFile.Name())
-			if err != nil {
-				t.Fatalf("failed to read CNI config file: %v", err)
-			}
-
-			if newContent != testcase.newCni {
-				t.Logf("actual CNI config: %v", newContent)
-				t.Logf("expected CNI config: %v", testcase.newCni)
-				t.Error("did not get expected CNI config content")
-			}
-		})
-	}
-}
 
 func Test_GetPodCidrFromNodeSpec(t *testing.T) {
 	testcases := []struct {
@@ -181,8 +71,11 @@ func Test_GetPodCidrFromNodeSpec(t *testing.T) {
 			if err != nil {
 				t.Fatalf("failed to create existing nodes for test: %v", err)
 			}
-
-			podCIDR, err := GetPodCidrFromNodeSpec(clientset, testcase.hostnameOverride)
+			node, err := GetNodeObject(clientset, testcase.hostnameOverride)
+			if err != nil {
+				t.Error("unable to get node object from API: " + err.Error())
+			}
+			podCIDR, err := GetPodCidrFromNodeSpec(node)
 			if !reflect.DeepEqual(err, testcase.err) {
 				t.Logf("actual error: %v", err)
 				t.Logf("expected error: %v", testcase.err)
@@ -198,21 +91,148 @@ func Test_GetPodCidrFromNodeSpec(t *testing.T) {
 	}
 }
 
-func createFile(content, filename string) (*os.File, error) {
-	file, err := os.Create(filename)
-	if err != nil {
-		return nil, fmt.Errorf("cannot create file: %v", err)
+func Test_GetPodCIDRsFromNodeSpec(t *testing.T) {
+	var blankCIDR []string
+	testcases := []struct {
+		name             string
+		hostnameOverride string
+		existingNode     *apiv1.Node
+		ipv4CIDRs        []string
+		ipv6CIDRs        []string
+		err              error
+	}{
+		{
+			"node with node.Spec.PodCIDRs",
+			"test-node",
+			&apiv1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+				},
+				Spec: apiv1.NodeSpec{
+					PodCIDRs: []string{
+						"172.17.0.0/24",
+						"2001:db8:42:0::/64",
+					},
+				},
+			},
+			[]string{"172.17.0.0/24"},
+			[]string{"2001:db8:42:0::/64"},
+			nil,
+		},
+		{
+			"node with multiple IPv4 addresses and no IPv6 in node.Spec.PodCIDRs",
+			"test-node",
+			&apiv1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+				},
+				Spec: apiv1.NodeSpec{
+					PodCIDRs: []string{
+						"172.17.0.0/24",
+						"172.18.0.0/24",
+					},
+				},
+			},
+			[]string{"172.17.0.0/24", "172.18.0.0/24"},
+			blankCIDR,
+			nil,
+		},
+		{
+			"node with multiple IPv6 addresses in node.Spec.PodCIDRs",
+			"test-node",
+			&apiv1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+				},
+				Spec: apiv1.NodeSpec{
+					PodCIDRs: []string{
+						"172.17.0.0/24",
+						"2001:db8:42:0::/64",
+						"2001:db8:42:1::/64",
+					},
+				},
+			},
+			[]string{"172.17.0.0/24"},
+			[]string{"2001:db8:42:0::/64", "2001:db8:42:1::/64"},
+			nil,
+		},
+		{
+			"node with node.Annotations['kube-router.io/pod-cidrs']",
+			"test-node",
+			&apiv1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+					Annotations: map[string]string{
+						podCIDRsAnnotation: "172.17.0.0/24,2001:db8:42:2::/64",
+					},
+				},
+			},
+			[]string{"172.17.0.0/24"},
+			[]string{"2001:db8:42:2::/64"},
+			nil,
+		},
+		{
+			"node with invalid pod IPv4 cidrs in node.Annotations['kube-router.io/pod-cidrs']",
+			"test-node",
+			&apiv1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+					Annotations: map[string]string{
+						podCIDRsAnnotation: "172.17.0.0,2001:db8:42:2::/64",
+					},
+				},
+			},
+			blankCIDR,
+			blankCIDR,
+			errors.New("error parsing pod CIDR in node annotation: invalid CIDR address: 172.17.0.0"),
+		},
+		{
+			"node with invalid pod IPv6 cidrs in node.Annotations['kube-router.io/pod-cidrs']",
+			"test-node",
+			&apiv1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test-node",
+					Annotations: map[string]string{
+						podCIDRsAnnotation: "172.17.0.0/24,2001:db8:42:2::",
+					},
+				},
+			},
+			[]string{"172.17.0.0/24"},
+			blankCIDR,
+			fmt.Errorf("error parsing pod CIDR in node annotation: invalid CIDR address: %s",
+				"2001:db8:42:2::"),
+		},
 	}
 
-	if _, err = file.Write([]byte(content)); err != nil {
-		return nil, fmt.Errorf("cannot write to file: %v", err)
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			clientset := fake.NewSimpleClientset()
+			_, err := clientset.CoreV1().Nodes().Create(context.Background(), testcase.existingNode, metav1.CreateOptions{})
+			if err != nil {
+				t.Fatalf("failed to create existing nodes for test: %v", err)
+			}
+			node, err := GetNodeObject(clientset, testcase.hostnameOverride)
+			if err != nil {
+				t.Error("unable to get node object from API: " + err.Error())
+			}
+			ipv4CIDRs, ipv6CIDRs, err := GetPodCIDRsFromNodeSpecDualStack(node)
+			if !reflect.DeepEqual(err, testcase.err) {
+				t.Logf("actual error: %v", err)
+				t.Logf("expected error: %v", testcase.err)
+				t.Error("did not get expected error")
+			}
+
+			if !reflect.DeepEqual(ipv4CIDRs, testcase.ipv4CIDRs) {
+				t.Logf("actual IPv4 podCIDR: %q", ipv4CIDRs)
+				t.Logf("expected IPv4 podCIDR: %q", testcase.ipv4CIDRs)
+				t.Error("did not get expected IPv4 podCIDR")
+			}
+
+			if !reflect.DeepEqual(ipv6CIDRs, testcase.ipv6CIDRs) {
+				t.Logf("actual IPv6 podCIDR: %q", ipv6CIDRs)
+				t.Logf("expected IPv6 podCIDR: %q", testcase.ipv6CIDRs)
+				t.Error("did not get expected IPv6 podCIDR")
+			}
+		})
 	}
-
-	fmt.Println("File is ", file.Name())
-	return file, nil
-}
-
-func readFile(filename string) (string, error) {
-	content, err := os.ReadFile(filename)
-	return string(content), err
 }
