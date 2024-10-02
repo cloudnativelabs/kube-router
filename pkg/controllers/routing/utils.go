463a514a3dfc8e640416ed515885379f208edd39
Aaron U'Ren <aauren@gmail.com>
2024-10-02 16:46:02 -0500

    fact(bgp): separate bgp functionality into new package

--- pkg/controllers/routing/utils.go
+++ pkg/controllers/routing/utils.go
@@ -2,22 +2,15 @@ package routing
 
 import (
 	"encoding/base64"
-	"encoding/binary"
-	"errors"
 	"fmt"
-	"hash/fnv"
 	"net"
-	"regexp"
 	"strconv"
 	"strings"
 
-	"github.com/cloudnativelabs/kube-router/v2/pkg/utils"
 	gobgpapi "github.com/osrg/gobgp/v3/api"
-	"github.com/osrg/gobgp/v3/pkg/packet/bgp"
+
 	v1core "k8s.io/api/core/v1"
 	"k8s.io/klog/v2"
-
-	"github.com/vishvananda/netlink"
 )
 
 // Used for processing Annotations that may contain multiple items
@@ -115,108 +108,6 @@ func statementsEqualByName(a, b []*gobgpapi.Statement) bool {
 
 	// If we've made it through both loops then we know that the statements arrays are equal
 	return true
-}
-
-// generateRouterID will generate a router ID based upon the user's configuration (or lack there of) and the node's
-// primary IP address if the user has not specified. If the user has configured the router ID as "generate" then we
-// will generate a router ID based upon fnv hashing the node's primary IP address.
-func generateRouterID(nodeIPAware utils.NodeIPAware, configRouterID string) (string, error) {
-	switch {
-	case configRouterID == "generate":
-		h := fnv.New32a()
-		h.Write(nodeIPAware.GetPrimaryNodeIP())
-		hs := h.Sum32()
-		gip := make(net.IP, 4)
-		binary.BigEndian.PutUint32(gip, hs)
-		return gip.String(), nil
-	case configRouterID != "":
-		return configRouterID, nil
-	}
-
-	if nodeIPAware.GetPrimaryNodeIP().To4() == nil {
-		return "", errors.New("router-id must be specified when primary node IP is an IPv6 address")
-	}
-	return configRouterID, nil
-}
-
-// validateCommunity takes in a string and attempts to parse a BGP community out of it in a way that is similar to
-// gobgp (internal/pkg/table/policy.go:ParseCommunity()). If it is not able to parse the community information it
-// returns an error.
-func validateCommunity(arg string) error {
-	_, err := strconv.ParseUint(arg, 10, bgpCommunityMaxSize)
-	if err == nil {
-		return nil
-	}
-
-	_regexpCommunity := regexp.MustCompile(`(\d+):(\d+)`)
-	elems := _regexpCommunity.FindStringSubmatch(arg)
-	if len(elems) == 3 {
-		if _, err := strconv.ParseUint(elems[1], 10, bgpCommunityMaxPartSize); err == nil {
-			if _, err = strconv.ParseUint(elems[2], 10, bgpCommunityMaxPartSize); err == nil {
-				return nil
-			}
-		}
-	}
-	for _, v := range bgp.WellKnownCommunityNameMap {
-		if arg == v {
-			return nil
-		}
-	}
-	return fmt.Errorf("failed to parse %s as community", arg)
-}
-
-// parseBGPNextHop takes in a GoBGP Path and parses out the destination's next hop from its attributes. If it
-// can't parse a next hop IP from the GoBGP Path, it returns an error.
-func parseBGPNextHop(path *gobgpapi.Path) (net.IP, error) {
-	for _, pAttr := range path.GetPattrs() {
-		unmarshalNew, err := pAttr.UnmarshalNew()
-		if err != nil {
-			return nil, fmt.Errorf("failed to unmarshal path attribute: %s", err)
-		}
-		switch t := unmarshalNew.(type) {
-		case *gobgpapi.NextHopAttribute:
-			// This is the primary way that we receive NextHops and happens when both the client and the server exchange
-			// next hops on the same IP family that they negotiated BGP on
-			nextHopIP := net.ParseIP(t.NextHop)
-			if nextHopIP != nil && (nextHopIP.To4() != nil || nextHopIP.To16() != nil) {
-				return nextHopIP, nil
-			}
-			return nil, fmt.Errorf("invalid nextHop address: %s", t.NextHop)
-		case *gobgpapi.MpReachNLRIAttribute:
-			// in the case where the server and the client are exchanging next-hops that don't relate to their primary
-			// IP family, we get MpReachNLRIAttribute instead of NextHopAttributes
-			// TODO: here we only take the first next hop, at some point in the future it would probably be best to
-			// consider multiple next hops
-			nextHopIP := net.ParseIP(t.NextHops[0])
-			if nextHopIP != nil && (nextHopIP.To4() != nil || nextHopIP.To16() != nil) {
-				return nextHopIP, nil
-			}
-			return nil, fmt.Errorf("invalid nextHop address: %s", t.NextHops[0])
-		}
-	}
-	return nil, fmt.Errorf("could not parse next hop received from GoBGP for path: %s", path)
-}
-
-// parseBGPPath takes in a GoBGP Path and parses out the destination subnet and the next hop from its attributes.
-// If successful, it will return the destination of the BGP path as a subnet form and the next hop. If it
-// can't parse the destination or the next hop IP, it returns an error.
-func parseBGPPath(path *gobgpapi.Path) (*net.IPNet, net.IP, error) {
-	nextHop, err := parseBGPNextHop(path)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	nlri := path.GetNlri()
-	var prefix gobgpapi.IPAddressPrefix
-	err = nlri.UnmarshalTo(&prefix)
-	if err != nil {
-		return nil, nil, fmt.Errorf("invalid nlri in advertised path")
-	}
-	dstSubnet, err := netlink.ParseIPNet(prefix.Prefix + "/" + fmt.Sprint(prefix.PrefixLen))
-	if err != nil {
-		return nil, nil, fmt.Errorf("couldn't parse IP subnet from nlri advertised path")
-	}
-	return dstSubnet, nextHop, nil
 }
 
 // getPodCIDRsFromAllNodeSources gets the pod CIDRs for all available sources on a given node in a specific order. The
