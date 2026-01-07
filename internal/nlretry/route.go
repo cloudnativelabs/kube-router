5ba705e97da025d84e3afad0a102a36c8f6acd14
Cat C <catherinetcai@users.noreply.github.com>
2026-01-06 21:08:49 -0800

    docs: Add GoDoc comments for all wrapper functions

--- internal/nlretry/route.go
+++ internal/nlretry/route.go
@@ -6,6 +6,9 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
+// RouteListFiltered is a wrapper around netlink.RouteListFiltered that retries with exponential backoff on
+// netlink.ErrDumpInterrupted. The retry can be interrupted and the result/error will be returned if the
+// context is cancelled.
 func RouteListFiltered(ctx context.Context, family int, filter *netlink.Route, filterMask uint64) ([]netlink.Route,
 	error) {
 	return RetryErrDumpInterruptedWithResult(ctx, netlinkRetryInterval, netlinkRetryMaxInterval, netlinkRetryAttempts,
